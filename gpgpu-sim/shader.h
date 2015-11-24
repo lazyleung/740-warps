5f6800c457e830b29a8652e373438b4dc6800eb0
miyengar <miyengar@andrew.cmu.edu>
2015-11-23 23:02:00 -0400

    add memory divergence statistics

--- gpgpu-sim/shader.h
+++ gpgpu-sim/shader.h
@@ -1464,6 +1464,9 @@ public:
 
         m_shader_dynamic_warp_issue_distro.resize( config->num_shader() );
         m_shader_warp_slot_issue_distro.resize( config->num_shader() );
+
+		m_load_exec = 0;
+		memset(m_load_count, 0, sizeof(m_load_count));
     }
 
     ~shader_core_stats()
@@ -1497,6 +1500,13 @@ public:
         return m_shader_warp_slot_issue_distro;
     }
 
+	void count_mem_divergence(unsigned count) {
+		if (count > 0) {
+			m_load_exec++;
+			m_load_count[count]++;
+		}
+	}
+
 private:
     const shader_core_config *m_config;
 
@@ -1508,6 +1518,10 @@ private:
     std::vector<unsigned> m_last_shader_dynamic_warp_issue_distro;
     std::vector< std::vector<unsigned> > m_shader_warp_slot_issue_distro;
     std::vector<unsigned> m_last_shader_warp_slot_issue_distro;
+
+	// memory divergence measure
+	unsigned m_load_exec;
+	unsigned m_load_count[MAX_WARP_SIZE];
 
     friend class power_stat_t;
     friend class shader_core_ctx;
